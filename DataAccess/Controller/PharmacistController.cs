e72e7591bc9f3fba9ca1ce0b9f2fbdc6a0cc6c3e
Md Ehsanul Haque <133691757+EhsanulHaqueSiam@users.noreply.github.com>
2024-09-23 19:48:45 +0600

    updated dao

--- DataAccess/Controller/PharmacistController.cs
+++ DataAccess/Controller/PharmacistController.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using PharmacyManagementSystem.DataAccess.DAO;
+using PharmacyManagementSystem.Model;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace PharmacyManagementSystem.DataAccess.Controller {
-    internal class PharmacistController {
+namespace PharmacyManagementSystem.Controllers {
+    public class PharmacistController {
+        private readonly IPharmacistDao _pharmacistDao;
+
+        public PharmacistController(IPharmacistDao pharmacistDao) {
+            _pharmacistDao = pharmacistDao;
+        }
+
+        public void InsertPharmacist(Pharmacist pharmacist) {
+            _pharmacistDao.Insert(pharmacist);
+        }
+
+        public Pharmacist GetPharmacistById(int id) {
+            return _pharmacistDao.GetById(id);
+        }
+
+        public List<Pharmacist> GetAllPharmacists() {
+            return _pharmacistDao.GetAll();
+        }
+
+        public void UpdatePharmacist(Pharmacist pharmacist) {
+            _pharmacistDao.Update(pharmacist);
+        }
+
+        public void DeletePharmacist(int id) {
+            _pharmacistDao.Delete(id);
+        }
     }
 }
--- DataAccess/DAO/PharmacistDaoImpl.cs
+++ DataAccess/DAO/PharmacistDaoImpl.cs
@@ -1,10 +1,94 @@
-﻿using System;
+﻿using PharmacyManagementSystem.DataAccess.Constants;
+using PharmacyManagementSystem.Model;
+using PharmacyManagementSystem.DataAccess.DAO;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 
-namespace PharmacyManagementSystem.DataAccess.DAO {
-    internal class PharmacistDaoImpl {
+namespace PharmacyManagementSystem.DataAccess {
+    public class PharmacistDaoImpl : IPharmacistDao {
+        public void Insert(Pharmacist pharmacist) {
+            SqlDatabaseManager.Instance.Execute(connection => {
+                using (var cmd = new SqlCommand(PharmacistQueries.InsertPharmacist, connection)) {
+                    cmd.Parameters.AddWithValue("@UserName", pharmacist.P_UserName);
+                    cmd.Parameters.AddWithValue("@Name", pharmacist.P_Name);
+                    cmd.Parameters.AddWithValue("@Dob", pharmacist.P_Dob);
+                    cmd.Parameters.AddWithValue("@Number", pharmacist.P_Number);
+                    cmd.Parameters.AddWithValue("@Pass", pharmacist.P_Pass);
+                    cmd.Parameters.AddWithValue("@Mail", pharmacist.P_Mail);
+
+                    cmd.ExecuteNonQuery();
+                }
+            });
+        }
+
+        public Pharmacist GetById(int id) {
+            return SqlDatabaseManager.Instance.Execute(connection => {
+                using (var cmd = new SqlCommand(PharmacistQueries.GetPharmacistById, connection)) {
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    using (var reader = cmd.ExecuteReader()) {
+                        if (reader.Read()) {
+                            return new Pharmacist {
+                                P_ID = (int)reader["P_ID"],
+                                P_UserName = reader["P_UserName"].ToString(),
+                                P_Name = reader["P_Name"].ToString(),
+                                P_Dob = (DateTime?)reader["P_Dob"],
+                                P_Number = reader["P_Number"].ToString(),
+                                P_Pass = reader["P_Pass"].ToString(),
+                                P_Mail = reader["P_Mail"].ToString()
+                            };
+                        }
+                        return null;
+                    }
+                }
+            });
+        }
+
+        public List<Pharmacist> GetAll() {
+            return SqlDatabaseManager.Instance.Execute(connection => {
+                var pharmacists = new List<Pharmacist>();
+                using (var cmd = new SqlCommand(PharmacistQueries.GetAllPharmacists, connection)) {
+                    using (var reader = cmd.ExecuteReader()) {
+                        while (reader.Read()) {
+                            pharmacists.Add(new Pharmacist {
+                                P_ID = (int)reader["P_ID"],
+                                P_UserName = reader["P_UserName"].ToString(),
+                                P_Name = reader["P_Name"].ToString(),
+                                P_Dob = (DateTime?)reader["P_Dob"],
+                                P_Number = reader["P_Number"].ToString(),
+                                P_Pass = reader["P_Pass"].ToString(),
+                                P_Mail = reader["P_Mail"].ToString()
+                            });
+                        }
+                    }
+                }
+                return pharmacists;
+            });
+        }
+
+        public void Update(Pharmacist pharmacist) {
+            SqlDatabaseManager.Instance.Execute(connection => {
+                using (var cmd = new SqlCommand(PharmacistQueries.UpdatePharmacist, connection)) {
+                    cmd.Parameters.AddWithValue("@Id", pharmacist.P_ID);
+                    cmd.Parameters.AddWithValue("@UserName", pharmacist.P_UserName);
+                    cmd.Parameters.AddWithValue("@Name", pharmacist.P_Name);
+                    cmd.Parameters.AddWithValue("@Dob", pharmacist.P_Dob);
+                    cmd.Parameters.AddWithValue("@Number", pharmacist.P_Number);
+                    cmd.Parameters.AddWithValue("@Pass", pharmacist.P_Pass);
+                    cmd.Parameters.AddWithValue("@Mail", pharmacist.P_Mail);
+
+                    cmd.ExecuteNonQuery();
+                }
+            });
+        }
+
+        public void Delete(int id) {
+            SqlDatabaseManager.Instance.Execute(connection => {
+                using (var cmd = new SqlCommand(PharmacistQueries.DeletePharmacist, connection)) {
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            });
+        }
     }
 }
